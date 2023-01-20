994121644a4b184410afc82c058a7644c07e439a
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2023-01-20 15:08:49 +0700

    Initial commit

---
+++ WPF_Money_Calculate/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Money_Calculate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_day_Click(object sender, RoutedEventArgs e)
+        {
+            int day = int.Parse(wishlist.Text) / (int.Parse(income.Text) - int.Parse(expenses.Text));
+            days.Text = day.ToString();
+            //income.Text = "รายได้";
+            //expenses.Text = "รายได้";
+            //wishlist.Text = "สิ่งที่อยากได้";
+            //days.Text = "วัน";
+        }
+    }
+}
